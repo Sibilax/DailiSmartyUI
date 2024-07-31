da1b5a219b6bf2ed3b823c40ef50366ba12c0943
Cecilia <ceci_2504@hotmail.com>
2024-08-01 01:46:29 +0200

    used axios to perform get request to fetch recents posts and setup the required reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,15 @@
+import {SET_RECENT_POSTS} from './types';
+import axios from 'axios';
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        //perform our request, aun no se puede pq hay que instalar redux thunk
-        console.log('hello');
+        axios.get('https://api.thedogapi.com/v1/breeds')
+            .then(response => {
+                console.log(response.data);// solicitud para dailysmarty: console.log(response.data.posts);
+                dispatch({
+                    type: SET_RECENT_POSTS,
+                    payload: response.data.posts
+                })
+            })
     }
 }

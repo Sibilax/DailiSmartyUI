fd39a90ee7ae1a543cf998b1a48da8dab4b0a8e1
Cecilia <ceci_2504@hotmail.com>
2024-07-31 16:45:18 +0200

    set up redux thunk for api requests, connected recentposts to redux, corrected component to recentposts

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export function fetchRecentPosts() {
+    return function(dispatch) {
+        //perform our request, aun no se puede pq hay que instalar redux thunk
+        console.log('hello');
+    }
+}

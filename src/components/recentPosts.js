148601cec6003c61cea4a1d335df5505b7320841
Cecilia <ceci_2504@hotmail.com>
2024-08-01 22:46:47 +0200

    added mapStateToProps(state){} in the recentPosts.js component and rendered the state.posts.recentPosts with li tags

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -9,20 +9,36 @@ class RecentPosts extends Component {
     this.props.fetchRecentPosts();
   }
 
+  renderPosts = function() {
+    return this.props.recentPosts.map((post, index) => {
+        return (
+            <li key={index}>  
+                {post.title} {/* luego lo cambiamos, de momento solo queremos verificar que funcione */}
+            </li>
+        )
+    })
+    return posts
+  }
+
   render() {
-    return (
+      return (
       <div className="recent-posts">
-        <div className="recent-posts_wrapper">
+          <div className="recent-posts_wrapper">
           <div className="recent-posts_heading">Recent Posts</div>
           <ul className="recent-posts_posts">
-            <li>recent post 0</li>
-            <li>recent post 1</li>
-            <li>recent post 2</li>
+              {this.renderPosts()}
           </ul>
-        </div>
+          </div>
       </div>
-    )
+      )
   }
 }
 
-export default connect(null, actions)(RecentPosts);
+
+  function mapStateToProps(state) {
+    return{
+        recentPosts: state.posts.recentPosts //esto nos da acceso a esos elementos en el state que vemos en redux devtools
+    }
+  }
+
+export default connect(mapStateToProps, actions)(RecentPosts);
